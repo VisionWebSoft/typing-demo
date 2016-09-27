5718de6ba624849e76bd09d996f38eac8309f349
Andrew <arhq315@gmail.com>
2016-09-27 16:25:41 -0500

    Created FIles
    Created inital files from a template.

---
+++ js/logic.js
@@ -0,0 +1,7 @@
+'use strict';
+window.logic=(function()
+{
+	var state={};
+	var obj={};
+	return obj;
+})();
---
+++ js/output.js
@@ -0,0 +1,6 @@
+'use strict';
+window.output={};
+output.init=function()
+{
+	mr.freeze(input,logic,output);
+};
